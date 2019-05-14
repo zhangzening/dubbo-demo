b3e7b14551bf47ade27185ccc2848adea6cc60dd
zaney <18600736674@163.com>
2019-05-14 19:10:48 +0800

    修改

--- dubbov2_7/dubbov2_7-demo-xml/dubbov2_7-demo-xml-consumer/src/main/java/com/dubbo/demo/consumer/config/DubboConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dubbo.demo.consumer.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:dubbo/*.xml")
-public class DubboConfig {
-}
--- dubbov2_7/dubbov2_7-demo-xml/dubbov2_7-demo-xml-provider-sleep/src/main/java/com/dubbo/demo/provider/config/DubboConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dubbo.demo.provider.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("classpath:dubbo/dubbo-provider.xml")
-public class DubboConfig {
-}
